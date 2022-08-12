7e55edb9c7c3a2870c8eac57ef0a88b72864241b
desguerra <ddesguerra@ucdavis.edu>
2022-08-12 12:26:39 -0700

    fetch API data from title and parse data

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -2,6 +2,31 @@
 
 var searchBarFormEl = document.querySelector("#search-bar-form"); // <form> ID
 var animeInputEl = document.querySelector("#anime-title"); // form <input> id
+var searchBarResEl = document.querySelector("#search-bar-results"); // search results <div> id
+
+var getAnimeData = function(title) {
+    var animeNewsUrl = "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~" + title;
+
+    fetch(animeNewsUrl).then(function(res) {
+        if (res.ok) {
+            // parse XML data
+            res.text().then(function(data) {
+                var parser = new DOMParser();
+                var xml = parser.parseFromString(data, "text/xml");
+                console.log(xml);
+            });
+        } 
+        else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    
+    .catch(function(error) {
+        // chain `.catch()` onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
+    });
+
+};
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -9,7 +34,7 @@ var formSubmitHandler = function(event) {
     var animeTitle = animeInputEl.value.trim();
 
     if (animeTitle) {
-        console.log(animeTitle);
+        getAnimeData(animeTitle);
 
         // reset form
         animeInputEl.value = "";
